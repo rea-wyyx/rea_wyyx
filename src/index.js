fb44feb05e416b0e409edb666862623f329bb40f
gossipphoto <577929827@qq.com>
2019-11-27 16:34:50 +0800

    11

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom'
 import { Provider } from "react-redux"
 import store from "./redux/store"
-import 'lib-flexible/flexible'
+ import 'lib-flexible/flexible'
 import './mock/mock-server'
 import App from './App';
 import history from './history'
